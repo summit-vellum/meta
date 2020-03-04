183217adb96a5ce48757d60f4a6fa675705a7308
Gien Mangaliman <Gien.Mangaliman@summitmedia.com.ph>
2020-03-04 13:30:00 +0800

    meta db schema

--- src/database/migrations/2020_03_04_031751_create_meta_table.php
+++ src/database/migrations/2020_03_04_031751_create_meta_table.php
@@ -15,7 +15,16 @@ class CreateMetaTable extends Migration
     {
         Schema::create('metas', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('title', 255);
+            $table->string('slug', 255);
+            $table->text('keyword')->nullable();
+            $table->text('description');
+            $table->tinyInteger('status')->default(1);
+            $table->string('post_type', 255);
+            $table->tinyInteger('post_id');
+            $table->string('canonical', 255)->nullable();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
